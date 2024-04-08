92e630a65aef09bb7bbd5f00ca3c069f89f0e169
dimitrioneal <159201294+dimitrioneal@users.noreply.github.com>
2024-04-07 19:43:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+ yaakovGreeter.sayHello();
+johnGreeter.sayBye();
+jenGreeter.sayBye();
+jasonGreeter.sayBye();
+paulGreeter.sayHello();
+frankGreeter.sayHello();
+larryGreeter.sayHello();
+paulaGreeter.sayHello();
+lauraGreeter.sayHello();
+jimGreeter.sayBye();
+
+
+
